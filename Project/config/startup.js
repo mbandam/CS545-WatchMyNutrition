16fce49903b8ab61b57a7b3fdc941cdae72442ca
Dhavala Manjunatha <dhavalamanjunatha@Dhavalas-Air.home>
2018-03-14 23:32:19 -0400

    Initial push. Login, Register, Profile pages working

---
+++ Project/config/startup.js
@@ -0,0 +1,82 @@
+const MongoClient = require("mongodb").MongoClient;
+const uuidv1 = require('uuid/v4');
+const users = "usersData"
+
+const settings = {
+    mongoConfig: {
+        serverUrl: "mongodb://localhost:27017/",
+        database: "effugio"
+    }
+};
+
+//NM - added email
+var makeDoc = function (user_id, name, hashedPassword, dob, gender, location, occupation, activity,
+     email,weight) {
+    return {
+        //_id: uuidv1(),
+        user_id: user_id,
+        name: name,
+        hashedPassword: hashedPassword,
+        dob: dob,
+        gender: gender,
+        location: location,
+        occupation: occupation,
+        email: email,
+        weight:weight
+
+    }
+};
+
+
+
+
+
+let fullMongoUrl = settings.mongoConfig.serverUrl + settings.mongoConfig.database;
+let _connection = undefined
+
+
+async function runSetup() {
+
+    // create db connection
+    const db = await MongoClient.connect(fullMongoUrl);
+
+    // adding travel data seed
+    //NM - Commenting the below collection drop line as starting the application gives 'MongoError: ns not found' error
+    //NM - According to lecturer's code dropping the database rather than a specific collection.
+    //await db.collection(travel).drop();
+    await db.dropDatabase();
+    
+    userCollection = await db.createCollection(users);
+
+
+    var userJack = makeDoc("jack_d", "Jack Dawson", "", "01/01/1990", "M", "Hoboken", "1","1",  "jack_d@gmail.com",150);
+    userJack.hashedPassword = "$2a$16$mEbkQxGZt3a/qidjcCb6O..ah9yyDlGRj2lWpSK/ebQJJjSp1ISmS";
+    userJack._id ="245b3e48-f959-464b-9615-b07d187d78a8";
+    
+    //password: password
+
+    var userRose = makeDoc("rose_d", "Rose Dewitt", "", "11/03/1995", "F", "Hoboken", "1","1","rose_d@gmail.com",130);
+        
+    userRose.hashedPassword = "$2a$16$biOzgZ.pj1lMO.sRg5MFZuAXLm5FCiWIuDu3hBO6.QXlEwrImJ28W";
+    userRose._id="fc650072-441c-43f0-b50d-d681294488c5";    
+    //password: password2
+
+    userlily = makeDoc("lilly", "Lilly Evans", "", "11/27/1989", "F", "Jerseycity", "Student","2","2","lilly_evans123@gmail.com",160);
+    userlily._id="ea8cbd84-3e7f-48e6-a728-9fa314e943a1";
+
+
+    usersList = [];
+    usersList.push(userJack);
+    usersList.push(userRose);
+    usersList.push(userlily);
+    
+    res = await userCollection.insertMany(usersList);
+    usersins = await userCollection.find().toArray();
+    // console.log(usersins);
+
+    return usersins;
+
+}
+
+
+var exports = module.exports = runSetup;
---
+++ Project/routes/users.js
@@ -0,0 +1,250 @@
+const express = require('express');
+const router = express.Router();
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+const data = require("../data");
+const userData = data.users;
+const travelData = data.travel;
+const budgetData = data.budget;
+const connectionData=data.connection;
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: 'watchmynuitrition',
+  api_key: '266718785839255',
+  api_secret: 'YoIwSpXNsaiRXNNB1EDp_gGmhOs'
+});
+var setCookie = require('set-cookie-parser');
+
+passport.use(new Strategy(
+  async function(username, password, cb) {
+      console.log("user: pass:"+username+" "+password);
+      var user= await userData.getUserbyUserId(username);
+      if(!user){
+          return cb(null, false, { message: 'Unknown User'});
+      }
+      var isMatch = await userData.comparePassword(password, user.hashedPassword);
+      if(isMatch){
+        return cb(null, user);
+      } else {
+            return cb(null, false, { message: 'Invalid password'});
+      }
+
+}));
+
+passport.serializeUser( function(user, cb) {
+    cb(null, user._id);
+  });
+  
+passport.deserializeUser(async function(id, cb) {
+  var user = await userData.getUser(id);
+  cb(null, user);
+  
+});
+
+
+router.get('/login',
+function(req, res) {
+  if (!req.isAuthenticated || !req.isAuthenticated()) {      
+    res.render('users/login', { message: req.flash('error') });    
+    
+  }else{
+    res.redirect('/profile');  
+  }
+});
+router.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn("/"),
+  async function (req, res) {
+    let errors = [];
+    
+   try {
+      
+      res.render('users/profile', {
+        errors: errors,
+        hasErrors: false,
+        updSuccess: false,
+        user: req.user
+      });
+    }
+    catch (e) {
+      errors.push(e);
+      res.render('users/profile', {
+        errors: errors,
+        hasErrors: true,
+        updSuccess: false,
+        user: req.user
+      });
+    }
+  });
+
+router.post("/profile", multipartMiddleware,async (req, res) => {
+    let updatedProfileData = req.body;
+  console.log("body: %j", req.body);
+  let errors = [];
+  
+
+  if ((updatedProfileData.newPwd) || (updatedProfileData.newPwdConfirm)){
+    if (updatedProfileData.newPwd !== updatedProfileData.newPwdConfirm){
+    //console.log("Coming into if");
+    errors.push("New Password and Confirm New Password don't match");
+    }
+  } 
+  if (errors.length > 0) {
+    //console.log("Inside errors.length if");
+    res.render('users/profile', {
+      errors: errors,
+      hasErrors: true,
+      updSuccess: false,
+      user: updatedProfileData
+    });
+    return;
+  }
+
+  try {
+    
+    let updatedUserProfile = await userData.updateUser(updatedProfileData);
+    res.render('users/profile', {
+      errors: errors,
+      hasErrors: false,
+      updSuccess: true,
+      user: updatedProfileData
+    });
+    return;
+  }
+  catch (e) {
+    //console.log("Inside catch");
+    //res.status(500).json({ error: e });
+    errors.push(e);
+    res.render('users/profile', {
+      errors: errors,
+      hasErrors: true,
+      updSuccess: false,
+      user: updatedProfileData
+    });
+  }
+});
+
+router.get('/dashboard',
+  require('connect-ensure-login').ensureLoggedIn("/"),
+  async function (req, res) {
+    
+   res.render('users/dashboard',{
+    
+    user: req.user,
+  
+    }
+      );
+    
+  });
+
+  
+router.post('/dashboard',
+  function (req, res) {
+
+  });
+
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/dashboard', failureRedirect: '/login', failureFlash: true }),
+  function (req, res) {
+    console.log('You are authenticated');
+    res.redirect('/profile');
+  });
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/login');
+});
+// Register
+router.get('/register', async function (req, res) {
+
+  try {
+    
+    res.render('users/register',  );
+  }
+  catch (e) {
+    response.status(500).json({ error: e });
+  }
+	
+});
+// Register User
+router.post('/register', multipartMiddleware, async function(req, res){
+
+	// Validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('gender', 'Gender is required').notEmpty().not().equals('Select Gender');
+    req.checkBody('activity', 'activity level is required').notEmpty().not().equals('Select activity level');
+    req.checkBody('occupation', 'Occupation is required').notEmpty().not().equals('Select Occupation');
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('weight', 'Weight is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('dob', 'Date of birth is required and should be a date').notEmpty();
+    req.checkBody('location', 'Location is required').notEmpty();
+    req.checkBody('user_id', 'Username is required').notEmpty();
+	  req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    var errors = req.validationErrors();
+  
+  
+  if(req.body.user_id){
+    user=await userData.getUserbyUserId(req.body.user_id);
+    if(user){
+        console.log("Username "+user.name+" already exists");
+        //let errorMessage="Username already exists";
+        let error_msg={"msg" :"Username already exists",
+        "param":"user_id" };
+        if(!errors){
+          errors=[];
+          errors.push(error_msg);
+          //console.log(typeof(errors));
+        }else{
+          errors.push(error_msg);
+        }
+    }
+}
+	if(errors){
+
+    var errors_user={
+      name:req.body.name ,
+      user_id:req.body.user_id,
+      password:req.body.password,
+      password2:req.body.password2,
+      dob:req.body.dob,
+      gender:req.body.gender,
+      location:req.body.location,
+      occupation:req.body.occupation,
+      activity:req.body.activity,
+      email:req.body.email ,
+      weight:req.body.weight
+    };
+
+    
+    res.render('users/register', {errors:errors,user:errors_user} );
+
+
+  } else {
+    
+    const newUser = {
+      user_id: req.body.user_id,
+      hashedPassword: "",
+      password: req.body.password,
+      name: req.body.name,
+      dob: req.body.dob,
+      gender: req.body.gender,
+      location: req.body.location,
+      occupation: req.body.occupation,
+      email: req.body.email,
+      activity:req.body.activity,
+      weight:req.body.weight
+    };
+    addedUser=await userData.addUser(newUser,newUser.password);
+    console.log("added new user");
+    console.log(addedUser);
+    
+    req.flash('success_msg', 'You are registered and can now login');
+    res.redirect('/users/login');  
+    
+	}
+});
+module.exports = router;
