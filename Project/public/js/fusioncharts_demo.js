32ae5a0e6760ed03479225b60913727142f9261f
Dhavala Manjunatha <dhavalamanjunatha@Dhavalas-Air.home>
2018-04-24 22:00:32 -0400

    adding weight to graph

--- Project/public/js/fusioncharts_demo.js
+++ Project/public/js/fusioncharts_demo.js
@@ -16,22 +16,76 @@ $(document).ready(function(){
     success : function(data) {
       chartData = data;
       
-
+      console.log("Corr" +data["dataset"][1].corr);
       var chartProperties = {
-        "caption": "Variation of your nutrition levels with date",
+        "caption": "Variation of your Nutrition levels and Weight with date",
         "xAxisName": "Date (mm/dd)",
-        "yAxisName": "Nutrition Value"
+        "pYAxisName": "Nutrition Value",
+        "sYAxisName": "Weight (in lbs)",
+        "pYAxisMaxValue": "10",
+        "pYAxisMinValue": "0",
+        "paletteColors": "#0075c2,#1aaf5d,#f2c500",
+        "captionFont": "Arial",
+        "captionFontSize": "35",
+        "captionFontColor": "#08648e",
+        "captionFontBold": "1",
+        "xAxisNameFont": "Arial",
+        "xAxisNameFontSize": "20",
+        "xAxisNameFontColor": "#08648e",
+        "xAxisNameFontBold": "1",
+        "xAxisNameFontItalic": "1",
+        "yAxisNameFont": "Arial",
+        "yAxisNameFontSize": "20",
+        "yAxisNameFontColor": "#08648e",
+        "yAxisNameFontBold": "1",
+        "yAxisNameFontItalic": "1",
+            "baseFontColor": "#333333",
+            "baseFont": "Helvetica Neue,Arial",
+            "captionFontSize": "14",
+            "subcaptionFontSize": "14",
+            "subcaptionFontBold": "0",
+            "showBorder": "0",
+            "bgColor": "#ffffff",
+            "showShadow": "0",
+            "canvasBgColor": "#ffffff",
+            "canvasBorderAlpha": "0",
+            "divlineAlpha": "100",
+            "divlineColor": "#999999",
+            "divlineThickness": "1",
+            "divLineIsDashed": "1",
+            "divLineDashLen": "1",
+            "divLineGapLen": "1",
+            "usePlotGradientColor": "0",
+            "showplotborder": "0",
+            "showXAxisLine": "1",
+            "xAxisLineThickness": "1",
+            "xAxisLineColor": "#999999",
+            "showAlternateHGridColor": "0",
+            "showAlternateVGridColor": "0",
+            "legendBgAlpha": "0",
+            "legendBorderAlpha": "0",
+            "legendShadow": "0",
+            "legendItemFontSize": "20",
+            "labelFontSize": "13",
+            "legendItemFontColor": "#666666",
+            "showBorder": "1",
+            "borderThickness": "2",
+            "borderAlpha": "80",
+            "bgColor": "#DDDDDD",
+            "bgAlpha": "50",
+            "setAdaptiveYMin":"1",
       };
 
+     
       var categoriesArray = [{
           "category" : data["categories"]
       }];
 
       var lineChart = new FusionCharts({
-        type: 'msline',
+        type: 'mscombidy2d',
         renderAt: 'chart-location',
-        width: '700',
-        height: '400',
+        width: '800',
+        height: '500',
         dataFormat: 'json',
         dataSource: {
           chart: chartProperties,
@@ -39,11 +93,14 @@ $(document).ready(function(){
           dataset : data["dataset"]
         }
       });
+
+     
       if(data["categories"].length > 0){
         lineChart.render();
         $("#error").hide();
       }
       else{
+        lineChart.render();
         $("#error").show();
       }
      
@@ -62,22 +119,75 @@ $(document).ready(function(){
     success : function(data) {
       chartData = data;
       
-
       var chartProperties = {
-        "caption": "Variation of your nutrition levels with date",
+        "caption": "Variation of your Nutrition levels and Weight with date",
         "xAxisName": "Date (mm/dd)",
-        "yAxisName": "Nutrition Value"
+        "pYAxisName": "Nutrition Value",
+        "sYAxisName": "Weight (in lbs)",
+        "pYAxisMaxValue": "10",
+        "pYAxisMinValue": "0",
+        "setAdaptiveYMin":"1",
+        "paletteColors": "#0075c2,#1aaf5d,#f2c500",
+        "captionFont": "Arial",
+        "captionFontSize": "35",
+        "captionFontColor": "#08648e",
+        "captionFontBold": "1",
+        "xAxisNameFont": "Arial",
+        "xAxisNameFontSize": "20",
+        "xAxisNameFontColor": "#08648e",
+        "xAxisNameFontBold": "1",
+        "xAxisNameFontItalic": "1",
+        "yAxisNameFont": "Arial",
+        "yAxisNameFontSize": "20",
+        "yAxisNameFontColor": "#08648e",
+        "yAxisNameFontBold": "1",
+        "yAxisNameFontItalic": "1",
+            "baseFontColor": "#333333",
+            "baseFont": "Helvetica Neue,Arial",
+            "captionFontSize": "14",
+            "subcaptionFontSize": "14",
+            "subcaptionFontBold": "0",
+            "showBorder": "0",
+            "bgColor": "#ffffff",
+            "showShadow": "0",
+            "canvasBgColor": "#ffffff",
+            "canvasBorderAlpha": "0",
+            "divlineAlpha": "100",
+            "divlineColor": "#999999",
+            "divlineThickness": "1",
+            "divLineIsDashed": "1",
+            "divLineDashLen": "1",
+            "divLineGapLen": "1",
+            "usePlotGradientColor": "0",
+            "showplotborder": "0",
+            "showXAxisLine": "1",
+            "xAxisLineThickness": "1",
+            "xAxisLineColor": "#999999",
+            "showAlternateHGridColor": "0",
+            "showAlternateVGridColor": "0",
+            "legendBgAlpha": "0",
+            "legendBorderAlpha": "0",
+            "legendShadow": "0",
+            "legendItemFontSize": "20",
+            "labelFontSize": "13",
+            "legendItemFontColor": "#666666",
+            "showBorder": "1",
+            "borderThickness": "2",
+        "borderAlpha": "80",
+        "bgColor": "#edeaea",
+        "bgAlpha": "50"
       };
 
+     
       var categoriesArray = [{
           "category" : data["categories"]
       }];
 
       var lineChart = new FusionCharts({
-        type: 'msline',
+        type: 'mscombidy2d',
         renderAt: 'chart-location',
-        width: '700',
-        height: '400',
+        width: '800',
+        height: '500',
         dataFormat: 'json',
         dataSource: {
           chart: chartProperties,
@@ -85,6 +195,8 @@ $(document).ready(function(){
           dataset : data["dataset"]
         }
       });
+
+    
       if(data["categories"].length > 0){
         lineChart.render();
         $("#error").hide();
